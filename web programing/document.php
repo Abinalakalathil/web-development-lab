1964d30c3d776a47acb97cc1d195d1b2c30c08f4
Abin Jacob <74697101+Abinalakalathil@users.noreply.github.com>
2021-03-01 15:54:09 +0530

    Add files via upload

---
+++ web programing/document.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<?php
+echo "Hello Abin. welcome"
+ ?>
+</Body>
+</Html
---
+++ web programing/new.php.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+echo "abin jacob";
+?> 
+
+</body>
+</html>
